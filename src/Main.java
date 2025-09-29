68264433596a10181881859cfd3440b38027eb59
egorik <mrlid28@gmail.com>
2025-09-29 22:35:28 +0300

    summ to N

--- src/Main.java
+++ src/Main.java
@@ -26,3 +26,14 @@ class checker {
         }
     }
 }
+class sum {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int sum = 0;
+        for (int i = 1; i<=n; i++) {
+            sum +=i;
+        }
+        System.out.println(sum);
+    }
+}
