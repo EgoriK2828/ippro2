302fa1f0e3c341a0057b3ae2a90480dce7bb809c
egorik <mrlid28@gmail.com>
2025-09-29 22:55:25 +0300

    Revert "dla testa reverta"
    This reverts commit 6d78ad9c97705b1ce78dc1a7eadcf90247c1e06d.

--- src/Main.java
+++ src/Main.java
@@ -31,14 +31,9 @@ class sum {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int sum = 0;
-        for (int i = 0; i<=n; i++) {
-            sum = i;
+        for (int i = 1; i<=n; i++) {
+            sum +=i;
         }
         System.out.println(sum);
-        //бесполезные сообщения
-        //чисто для реверта
-        //
-         //а я еще и код поломал
-        //дурень 
     }
 }
