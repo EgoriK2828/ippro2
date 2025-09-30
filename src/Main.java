0f8df397371d7582a484378bb15ca6f1555c2fe4
egorik <mrlid28@gmail.com>
2025-09-30 15:59:44 +0300

    новая ветка feature

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 class HelloWorld {
     public static void main(String[] args) {
-        String text = "Привет, мир!, Hello Ekaterina Alekseevna";
+        String text = "Привет, мир!, Hello Ekaterina Alekseevna, this is a feature branch";
         try (OutputStreamWriter writer = new OutputStreamWriter(System.out, StandardCharsets.UTF_8)) {
             writer.write(text);
             writer.flush(); // Убедимся, что всё записано
